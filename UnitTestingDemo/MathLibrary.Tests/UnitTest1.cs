dc60784d6068de8f5c0733b4fc567f96bad4ff64
reem1511 <reemsaqer420@gmail.com>
2025-06-14 21:52:39 +0300

    function Divide, IsEnen

---
+++ UnitTestingDemo/MathLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿namespace MathLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+         [Fact]
+    public void IsEven_EvenNumber_ReturnsTrue()
+    {
+        bool result = Calculator.IsEven(8);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsEven_OddNumber_ReturnsFalse()
+    {
+        bool result = Calculator.IsEven(7);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsEven_OddNumber_ReturnsTrue()
+    {
+        bool result = Calculator.IsEven(0);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Divide_twoPositiveNumbers_ReturnsCorrectResult()
+    {
+        int result = Calculator.Divide(10, 2);
+        Assert.Equal(5 ,result);
+    }
+
+    [Fact]
+    public void Divide_NegativeAndPositive_ReturnsNegative()
+    {
+        int result = Calculator.Divide(-9, 3);
+        Assert.Equal(-3 ,result);
+    }
+
+    [Fact]
+    public void Divide_ByZero_ThrowsDivideByZeroException()
+    {
+        Assert.throws<DivideByZeroException>(() => Calculator.Divide(10, 0));
+    }
+    
+}
---
+++ UnitTestingDemo/MathLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace MathLibrary;
+
+public class Class1
+{
+   public static int Multiply(int a,int b)
+    {
+        return a*b;
+    }
+
+    public static int Divide(int a ,int b)
+    {
+        if(b == 0)
+           throw new DivideByZeroException("لايمكن القسمة على 0")
+        return a / b;
+    }
+
+     public static bool IsEven(int a)
+    {
+        return a % 2 == 0;
+    }
+}
